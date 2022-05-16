502f681b879e2dff0f38152e21a5b2e84d5331f5
twdeveloper <twdeveloper@outlook.com>
2022-05-16 14:52:28 +0800

    console bmi v1

---
+++ BasicConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace BasicConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int height = 0, weight = 0;
+
+            // Set console encoding to unicode
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            //輸入身高
+            Console.Write("請輸入身高(cm):");
+            var tmp = Console.ReadLine();
+            height = int.Parse(tmp);
+
+            //輸入體重
+            Console.Write("請輸入體重(kg):");
+            tmp = Console.ReadLine();
+            weight = int.Parse(tmp);
+
+            //身高轉公尺
+            float h = (float)height / 100;
+
+            //計算BMI
+            float BMI = weight / (h * h);
+            Console.WriteLine($"BMI:{BMI}");
+        }
+    }
+}
