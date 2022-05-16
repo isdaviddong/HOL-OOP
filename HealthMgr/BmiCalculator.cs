768f22611fcf5599a05b8b5ea1a52ffc23c82a59
twdeveloper <twdeveloper@outlook.com>
2022-05-16 20:01:10 +0800

    加入 get & set

--- HealthMgr/BmiCalculator.cs
+++ HealthMgr/BmiCalculator.cs
@@ -4,15 +4,38 @@ namespace HealthMgr
 {
     public class BmiCalculator
     {
-        public int Weight;
-        public int Height;
+        private int _Weight = 0;
+        public int Weight
+        {
+            get { return _Weight; }
+            set
+            {
+                if (value <= 0)
+                    throw new Exception("體重不合法!");
+                else
+                    _Weight = value;
+            }
+        }
+
+        private int _Height = 0;
+        public int Height
+        {
+            get { return _Height; }
+            set
+            {
+                if (value <= 0)
+                    throw new Exception("身高不合法!");
+                else
+                    _Height = value;
+            }
+        }
         public float BMI { get { return Calculate(); } }
 
         public float Calculate()
         {
             float result = 0;
 
-            float height = (float)Height / 100;
+            float height =(float) Height / 100;
             result = Weight / (height * height);
 
             return result;
