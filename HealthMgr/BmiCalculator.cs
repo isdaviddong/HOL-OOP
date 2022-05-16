83c7f8df0ef39be4364ef67c919eded3680c91b1
twdeveloper <twdeveloper@outlook.com>
2022-05-16 19:24:32 +0800

    BMI Class V1

---
+++ HealthMgr/BmiCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HealthMgr
+{
+    public class BmiCalculator
+    {
+        public int Weight;
+        public int Height;
+        public float BMI { get { return Calculate(); } }
+
+        public float Calculate()
+        {
+            float result = 0;
+
+            float height = Height / 100;
+            result = Weight / (height * height);
+
+            return result;
+        }
+    }
+}
